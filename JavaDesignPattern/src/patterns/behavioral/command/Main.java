69416f530a0253929817d98aa845a649f1228b77
ywyi1992 <ywyi1992@gmail.com>
2021-11-07 15:51:43 +0900

    2021-11-07 java project init

---
+++ JavaDesignPattern/src/patterns/behavioral/command/Main.java
@@ -0,0 +1,10 @@
+package patterns.behavioral.command;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaDesignPattern/src/patterns/structural/proxy/MainDecorator.java
@@ -0,0 +1,10 @@
+package patterns.structural.proxy;
+
+public class MainDecorator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
