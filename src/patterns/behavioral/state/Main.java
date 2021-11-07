de5e89eda031656af94aebeaa0ae2771a18f5c0a
ywyi1992 <ywyi1992@gmail.com>
2021-11-07 15:47:35 +0900

    2021-11-07 java design pattern project init

---
+++ src/patterns/behavioral/state/Main.java
@@ -0,0 +1,10 @@
+package patterns.behavioral.state;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/patterns/behavioral/strategy/Main.java
@@ -0,0 +1,10 @@
+package patterns.behavioral.strategy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
